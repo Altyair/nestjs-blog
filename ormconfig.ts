1e1ee72a0cab1720d9d5e0f9887229d32d3282a9
dzhumaniyazovdm <gitlab@xrm.ru>
2024-05-21 14:05:27 +0300

    first commit

---
+++ ormconfig.ts
@@ -0,0 +1,21 @@
+import { DataSourceOptions, DataSource } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 6703,
+    username: 'root',
+    password: '123',
+    database: 'blog',
+    synchronize: false,
+    bigNumberStrings: true,
+    multipleStatements: true,
+    logging: true,
+    entities: ['**/*.entity{ .ts,.js}'],
+    migrations: ['dist/src/modules/**/db/migrations/*{.ts,.js}'],
+    migrationsRun: false,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
